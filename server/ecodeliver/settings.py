535e3dbe80a8ee18982979a5d1ad7c5563e09c5c
Himanshu Kumar <kumarh664@gmail.com>
2024-03-25 23:57:04 +0530

    fix: dotenv -> django-environ

--- server/ecodeliver/settings.py
+++ server/ecodeliver/settings.py
@@ -14,9 +14,10 @@ from datetime import timedelta
 from pathlib import Path
 
 import os
-from dotenv import load_dotenv
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
